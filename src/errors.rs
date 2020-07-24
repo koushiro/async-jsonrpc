0c7ce4da94be328ca1c2dc40cd0f801530b528aa
Qinxuan Chen <koushiro.cqx@gmail.com>
2020-07-24 17:52:06 +0800

    Fix http and ws features (#8)
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- src/errors.rs
+++ src/errors.rs
@@ -1,5 +1,5 @@
 /// A result type that wraps up the rpc client errors.
-pub type Result<T> = std::result::Result<T, RpcError>;
+pub type Result<T, E = RpcError> = std::result::Result<T, E>;
 
 /// The error type for rpc client.
 #[derive(Debug, thiserror::Error)]
@@ -7,13 +7,15 @@ pub enum RpcError {
     /// Json serialization/deserialization error.
     #[error("{0}")]
     Json(#[from] serde_json::Error),
-    /// HTTP error.
-    #[error("{0}")]
-    Http(#[from] reqwest::Error),
-    /// WebSocket error.
-    #[error("{0}")]
-    WebSocket(#[from] async_tungstenite::tungstenite::Error),
     /// Rpc request error, return failure response.
     #[error("{0}")]
     RpcResponse(#[from] crate::types::Error),
+    /// HTTP error.
+    #[cfg(feature = "http")]
+    #[error("{0}")]
+    Http(#[from] reqwest::Error),
+    /// WebSocket error.
+    #[cfg(feature = "ws")]
+    #[error("{0}")]
+    WebSocket(#[from] async_tungstenite::tungstenite::Error),
 }
