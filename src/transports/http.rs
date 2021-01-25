4195bc160c73137973e30262eef40f2ab794e398
koushiro <koushiro.cqx@gmail.com>
2021-01-25 15:42:18 +0800

    Add some tests
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- src/transports/http.rs
+++ src/transports/http.rs
@@ -238,3 +238,79 @@ impl Transport for HttpTransport {
         self.send_request(request).await
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn http_basic_auth() {
+        let builder = HttpTransportBuilder::new().basic_auth("username", Some("password"));
+        let basic_auth = builder.headers.get(header::AUTHORIZATION).unwrap();
+        assert_eq!(
+            basic_auth,
+            HeaderValue::from_static("Basic dXNlcm5hbWU6cGFzc3dvcmQ=")
+        );
+
+        let builder = HttpTransportBuilder::new().basic_auth("username", Option::<String>::None);
+        let basic_auth = builder.headers.get(header::AUTHORIZATION).unwrap();
+        assert_eq!(basic_auth, HeaderValue::from_static("Basic dXNlcm5hbWU6"));
+
+        let builder = HttpTransportBuilder::new().basic_auth("", Some("password"));
+        let basic_auth = builder.headers.get(header::AUTHORIZATION).unwrap();
+        assert_eq!(basic_auth, HeaderValue::from_static("Basic OnBhc3N3b3Jk"));
+    }
+
+    #[test]
+    fn http_bearer_auth() {
+        let builder = HttpTransportBuilder::new().bearer_auth("Hold my bear");
+        let bearer_auth = builder.headers.get(header::AUTHORIZATION).unwrap();
+        assert_eq!(bearer_auth, HeaderValue::from_static("Bearer Hold my bear"));
+    }
+
+    use hyper::{
+        body::{Body, HttpBody as _},
+        service::{make_service_fn, service_fn},
+        Method, Request as HttpRequest, Response as HttpResponse,
+    };
+
+    async fn server_v2(req: HttpRequest<Body>) -> hyper::Result<HttpResponse<Body>> {
+        assert_eq!(req.method(), &Method::POST);
+        assert_eq!(req.uri().path(), "/");
+        let mut content = vec![];
+        let mut body = req.into_body();
+        while let Some(Ok(chunk)) = body.data().await {
+            content.extend(&*chunk);
+        }
+
+        let expected = r#"{"jsonrpc":"2.0","method":"foo","params":[],"id":1}"#;
+        assert_eq!(std::str::from_utf8(&content), Ok(expected));
+
+        let response = r#"{"jsonrpc":"2.0","id":1,"result":"x"}"#;
+        Ok(HttpResponse::new(response.into()))
+    }
+
+    #[tokio::test]
+    async fn make_jsonrpc_v2_request() {
+        let addr = "127.0.0.1:8080";
+
+        let service = make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(server_v2)) });
+        let server = hyper::Server::bind(&addr.parse().unwrap()).serve(service);
+        tokio::spawn(server);
+
+        let client = HttpTransport::new(format!("http://{}", addr));
+        let response = client
+            .send("foo", Some(Params::Array(vec![])))
+            .await
+            .unwrap();
+        assert_eq!(
+            response,
+            SuccessResponse {
+                jsonrpc: Some(Version::V2_0),
+                result: Value::String("x".to_string()),
+                id: Id::Num(1),
+            }
+            .into()
+        );
+    }
+}
